7d40317f4e78a22f947f4b4abb972f7da5dec4e2
wcolts2000 <wcolts2000@gmail.com>
2019-08-14 21:23:37 -0700

    added custom 404 page and styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,30 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./404.css"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <main className="content">
+      <div className="err-wrapper">
+        <div className="text">
+          <p>
+            Ohhhh noooooo.....
+            <br /> It looks like we can't find what you are looking for
+            <br /> Get back to the shop here
+          </p>
+          <div className="wow pulse" data-wow-delay="1s" data-wow-iteration="7">
+            <Link to="/shop/" className="err-404-shop">
+              ENTER SHOP
+            </Link>
+          </div>
+        </div>
+        <h1 className="err-404 ">404</h1>
+      </div>
+    </main>
   </Layout>
 )
 
